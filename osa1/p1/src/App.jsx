16c7b639d0a76744ddabc76e83815c5e41e91ce9
kkarttun <kai.karttunen@psil.fi>
2026-02-12 16:41:59 +0200

    p1.4 palautus

--- osa1/p1/src/App.jsx
+++ osa1/p1/src/App.jsx
@@ -18,24 +18,26 @@ const Total = ({amounts}) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} ></Header>
-      <Content Content={[part1, part2, part3]}></Content>
-      {<Total amounts={[part1.exercises, part2.exercises, part3.exercises]}></Total>}
+      <Content Content={parts}></Content>
+      {<Total amounts={[parts[0].exercises, parts[1].exercises, parts[2].exercises]}></Total>}
     </div>
   )
 }
