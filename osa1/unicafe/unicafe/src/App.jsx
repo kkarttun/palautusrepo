4c53574654092894e5cf99e16edeb3b7fa781b9c
kkarttun <kai.karttunen@psil.fi>
2026-02-25 13:36:04 +0200

    p1.8 palautus

--- osa1/unicafe/unicafe/src/App.jsx
+++ osa1/unicafe/unicafe/src/App.jsx
@@ -8,40 +8,37 @@ const Display=({text, number, end}) =>{
   return <div>{text} {number} {end}</div>
 }
 
+const Statistics=({bad, neutral, good}) =>{
+  const all = good + neutral + bad;
+  const average = !all ? 0 : (good - bad) / all;
+  const positive = !all ? 0 : 100 * good / all;
+  return <>
+    <Header head = 'statistics'></Header>
+    <Display text="good" number={good} />
+    <Display text="neutral" number={neutral} />
+    <Display text="bad" number={bad} />
+    <Display text="all" number = {all}></Display>
+    <Display text="average" number = {average}></Display>
+    <Display text="positive" number = {positive} end='%'></Display>
+  </>
+}
+
 const Button = ({ state, onClick }) => {
   return <button onClick={() => onClick(state)}>{state}</button>
 }
 
 function App() {
-  const [reviews, setReviews] = useState({
-    good : 0,
-    neutral : 0,
-    bad : 0,
-  })
-  function handleReviews(state){
-    setReviews(prev => ({
-    ...prev,
-    [state]: prev[state] + 1
-    }));
-  }
-
-  const all = reviews.good + reviews.neutral + reviews.bad;
-  const average = !all ? 0 : (reviews.good - reviews.bad) / all;
-  const positive = !all ? 0 : 100 * reviews.good / all;
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <>
       <Header head = 'give feedback'></Header>
-      <Button state="good" onClick={handleReviews} />
-      <Button state="neutral" onClick={handleReviews} />
-      <Button state="bad" onClick={handleReviews} />
-      <Header head = 'statistics'></Header>
-      <Display text="good" number={reviews.good} />
-      <Display text="neutral" number={reviews.neutral} />
-      <Display text="bad" number={reviews.bad} />
-      <Display text="all" number={all} />
-      <Display text="average" number={average} />
-      <Display text="positive" number={positive} end="%"/>
+      <Button state="good" onClick={() => setGood(good + 1)} />
+      <Button state="neutral" onClick={() => setNeutral(neutral + 1)} />
+      <Button state="bad" onClick={() => setBad(bad + 1)} />
+      <Statistics {...{bad, neutral, good}}/>
     </>
   )
 }
