7efdd939c2a657bdb9539af9228d9baa2f15456f
kkarttun <kai.karttunen@psil.fi>
2026-02-26 12:57:49 +0200

    p2.6 palautus

---
+++ osa2/phonebook/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas'}
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    event.preventDefault();
+    if (!newName) return
+    setPersons(persons.concat({name: newName}));
+    setNewName('');
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value = {newName} onChange={name => setNewName(name.target.value)}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>{persons.map(person => (<div key = {person.name} >{person.name}</div>))}</div>
+    </div>
+  )
+}
+
+export default App
