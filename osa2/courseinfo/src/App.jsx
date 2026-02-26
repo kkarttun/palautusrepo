e4d93d8dfda7753d836aba6290884bab626b96be
kkarttun <kai.karttunen@psil.fi>
2026-02-26 10:24:24 +0200

    p2.2 palautus

--- osa2/courseinfo/src/App.jsx
+++ osa2/courseinfo/src/App.jsx
@@ -4,10 +4,15 @@ const Header = ({course}) => {
 const Content = ({parts}) => {
   return parts.map(part => <div key = {part.id}>{part.name} {part.exercises}</div>)
 }
+const Sum = ({parts}) => {
+  const total = parts.reduce((acc, part) => acc + part.exercises, 0);
+  return <b>total of {total} exercises</b>
+}
 const Course = ({course}) => {
   return <>
     <h1>{course.name}</h1>
     <Content parts = {course.parts}></Content>
+    <Sum parts={course.parts}></Sum>
   </>
 }
 
@@ -30,7 +35,7 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
-      },
+      }
     ]
   }
 
