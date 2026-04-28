d556eb3e8fce5ee594a2e1ca789a06656b19c834
kkarttun <kai.karttunen@psil.fi>
2026-04-28 13:56:19 +0300

    asdaskd

--- osa3/phonebookBackend/index.js
+++ osa3/phonebookBackend/index.js
@@ -3,6 +3,10 @@ const app = express()
 app.use(express.json())
 app.use(express.static('"../frontend/dist"'))
 
+const cors = require('cors')
+
+app.use(cors())
+
 const morgan = require('morgan')
 
 morgan.token('body', (request) => {
